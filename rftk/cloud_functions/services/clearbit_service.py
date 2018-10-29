c8b53d7910a8ed6a4ecb09f470c799658b65026b
jason wolosonovich <jmwoloso@asu.edu>
2018-10-29 12:19:19 -0700

    BUG: another bugfix in person payload function (clearbit service)

--- rftk/cloud_functions/services/clearbit_service.py
+++ rftk/cloud_functions/services/clearbit_service.py
@@ -318,15 +318,17 @@ def make_clearbit_person_gcs_payload(request=None):
     p["gravatar_handle"] = r["gravatar"]["handle"]
     p["gravatar_url_titles"] = list()
     p["gravatar_urls"] = list()
-    for d in r["gravatar"]["urls"]:
-        p["gravatar_url_titles"].append(d["value"])
-        p["gravatar_urls"].append(d["title"])
+    if len(r["gravatar"]["urls"]) != 0:
+        for d in r["gravatar"]["urls"]:
+            p["gravatar_url_titles"].append(d["value"])
+            p["gravatar_urls"].append(d["title"])
     p["gravatar_avatar"] = r["gravatar"]["avatar"]
     p["gravatar_avatar_types"] = list()
     p["gravatar_avatar_urls"] = list()
-    for d in r["gravatar"]["avatars"]:
-        p["gravatar_avatar_types"].append(d["type"])
-        p["gravatar_avatar_urls"].append(d["url"])
+    if len(r["gravatar"]["avatars"]) != 0:
+        for d in r["gravatar"]["avatars"]:
+            p["gravatar_avatar_types"].append(d["type"])
+            p["gravatar_avatar_urls"].append(d["url"])
 
     p["fuzzy_match"] = r["fuzzy"]
     p["is_email_provider"] = r["emailProvider"]
