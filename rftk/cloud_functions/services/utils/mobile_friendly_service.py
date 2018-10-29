7170dfa2717d8d53e8e94772d15637b74234c5bf
jason wolosonovich <jmwoloso@asu.edu>
2018-10-28 18:30:59 -0700

    ENH: added mobile_friendly_service functions

---
+++ rftk/cloud_functions/services/utils/mobile_friendly_service.py
@@ -0,0 +1,46 @@
+import requests
+
+
+def check_mobile_friendly(request=None):
+    """Utility function to test whether the supplied domain is mobile
+        friendly."""
+    print("_check_mobile_friendly()")
+    headers = {"Content-Type": "application/json"}
+    json_data = {"url": request["url"]}
+    api_url = \
+        "https://searchconsole.googleapis.com/v1/urlTestingTools" \
+        "/mobileFriendlyTest:run?key" \
+        "=AIzaSyCl4UW5RRovdGSf0xyjlwMpJ754n4_CcvM"
+    print("sending request for: {}".format(request["url"]))
+    resp = requests.post(
+        url=api_url,
+        headers=headers,
+        json=json_data
+    )
+
+    print("response: {}".format(resp))
+
+    resp_ = {
+        "refinery_id": request["refinery_id"],
+        "refined_at": request["refined_at"],
+        "refined_date": request["refined_date"],
+        "domain": request["domain"],
+        "url": request["url"]
+    }
+
+    # tests passed successfully
+    if resp.json()["testStatus"]["status"] == "COMPLETE":
+        resp_["test_results"] = resp.json()[
+            "mobileFriendliness"].lower()
+
+    # even errors seem to return a 200 status code?
+    elif resp.json()["testStatus"]["status"] != "COMPLETE":
+        if "details" in resp.json()["testStatus"].keys():
+            resp_["test_results"] = \
+                resp.json()["testStatus"]["status"] + \
+                " : " + \
+                resp.json()["testStatus"]["details"]
+        else:
+            resp_["test_results"] = \
+                resp.json()["testStatus"]["status"]
+    return resp_
